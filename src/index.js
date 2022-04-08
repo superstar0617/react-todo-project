f7ae41a581f27992e23028f6640d203b31fc3b92
superstar0617 <sulinane12@gmail.com>
2022-04-08 01:10:55 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
